187b6fc2cdfd8bcdce4d99e188aa5816a9f59082
Rodda Sangetha <136985592+RoddaSangeetha89@users.noreply.github.com>
2025-04-12 00:51:12 +0530

    Update Day1_KthFactorOfN.java

--- Week2_BasicMath_Logic/Day1_KthFactorOfN.java
+++ Week2_BasicMath_Logic/Day1_KthFactorOfN.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int kthFactor(int n, int k) {
+        int i=0;
+        for(int j=1;j<=n;j++)
+        {
+            if(n%j==0)
+            {
+                i++;
+            }
+            if(i==k)
+            {
+                return j;
+            }
+        }
+        return -1;
+    }
+}
