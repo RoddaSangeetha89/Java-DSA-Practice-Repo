cd8a53f4d8babfd917ed666151272de36d24cb33
Rodda Sangetha <136985592+RoddaSangeetha89@users.noreply.github.com>
2025-04-12 00:48:56 +0530

    Update RichestCustomerWealth.java

--- Week1_Arrays_Strings/RichestCustomerWealth.java
+++ Week1_Arrays_Strings/RichestCustomerWealth.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxw=0,sum;
+        for(int i=0;i<accounts.length;i++)
+        {
+            sum=0;
+            for(int j=0;j<accounts[i].length;j++)
+            {
+                sum=sum+accounts[i][j];
+            }
+            maxw=Math.max(maxw,sum);
+        }
+        return maxw;
+        
+    }
+}
